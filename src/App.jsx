e177b7c6f05b9822f01d5beaeceadb9d5611fc43
Daniel Cruz <nox.sb@hotmail.com>
2022-10-28 18:35:16 -0500

    Initial code

--- src/App.jsx
+++ src/App.jsx
@@ -4,52 +4,19 @@ import { HomePage } from "./pages/HomePage";
 import { SignInPage } from "./pages/SignInPage";
 import { DashboardPage } from "./pages/DashboardPage";
 
-const ProtectedRoute = ({ user, children }) => {
-  if (!user) {
-    return <Navigate to="/" replace />;
-  }
-
-  return children;
-};
-
 function App() {
-  const [user, setUser] = useState(null);
-
-  const login = () => {
-    setUser({ name: "Daniel" });
-  };
-
-  const logout = () => {
-    setUser(null);
-  };
-
   return (
     <BrowserRouter>
       <nav className="w-max mx-auto space-x-4 py-10 text-xl font-bold">
         <Link to="/">Home</Link>
         <Link to="/sign-in">Sign In</Link>
         <Link to="/dashboard">Dashboard</Link>
-        {user && (
-          <button
-            className="bg-red-300 text-red-900 py-2 px-6 rounded-md text-base"
-            onClick={logout}
-          >
-            Log Out
-          </button>
-        )}
       </nav>
       <main className="max-w-screen-lg mx-auto">
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="/sign-in" element={<SignInPage login={login} />} />
-          <Route
-            path="/dashboard"
-            element={
-              <ProtectedRoute user={user}>
-                <DashboardPage />
-              </ProtectedRoute>
-            }
-          />
+          <Route path="/sign-in" element={<SignInPage />} />
+          <Route path="/dashboard" element={<DashboardPage />} />
         </Routes>
       </main>
     </BrowserRouter>
--- src/pages/SignInPage.jsx
+++ src/pages/SignInPage.jsx
@@ -1,13 +1,10 @@
 import React from "react";
 
-export const SignInPage = ({ login }) => {
+export const SignInPage = () => {
   return (
     <>
       <h1>Sign In</h1>
-      <button
-        className="bg-purple-300 text-purple-900 py-2 px-6 rounded-md cursor-pointer"
-        onClick={login}
-      >
+      <button className="bg-purple-300 text-purple-900 py-2 px-6 rounded-md cursor-pointer">
         Login
       </button>
     </>
